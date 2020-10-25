d7063490385d55f8cfb5df0fbe4f3cf4df782520
J-iSkeLo <jiskelo@gmail.com>
2020-10-25 22:01:28 +0200

    Адаптер, конец

--- app/src/main/java/l/chernenkiy/aqua/Equipment/EquipmentAdapter.java
+++ app/src/main/java/l/chernenkiy/aqua/Equipment/EquipmentAdapter.java
@@ -1,5 +1,7 @@
 package l.chernenkiy.aqua.Equipment;
 
+import android.app.LauncherActivity;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,9 +10,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 import l.chernenkiy.aqua.R;
 
 public class EquipmentAdapter extends RecyclerView.Adapter<EquipmentAdapter.ViewHolder> {
+
+    private List<ItemEquipment> itemEquipmentsList;
+    private Context context;
+
+    public EquipmentAdapter(List<ItemEquipment> itemEquipmentsList, Context context) {
+        this.itemEquipmentsList = itemEquipmentsList;
+        this.context = context;
+    }
 
     @NonNull
     @Override
@@ -22,20 +34,28 @@ public class EquipmentAdapter extends RecyclerView.Adapter<EquipmentAdapter.View
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ItemEquipment listItem = itemEquipmentsList.get(position);
+
+        holder.category.setText(listItem.getCategory());
+        holder.numbCategory.setText(itemEquipmentsList.size());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return itemEquipmentsList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         public TextView category;
+        public TextView numbCategory;
 
         public ViewHolder(View itemView) {
             super(itemView);
+
+            category = itemView.findViewById(R.id.name_category);
+            numbCategory = itemView.findViewById(R.id.numb_category);
         }
     }
 }
