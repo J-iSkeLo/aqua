711fa1b69427ef56dc37a199383ea77f5336f1a5
login <jiskelo@gmail.com>
2021-05-20 15:50:57 +0300

    коррекктировки кода

--- app/src/main/java/l/chernenkiy/aqua/ShoppingBasket/FishBasket.java
+++ app/src/main/java/l/chernenkiy/aqua/ShoppingBasket/FishBasket.java
@@ -9,11 +9,9 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -54,6 +52,7 @@ public class FishBasket extends Fragment {
     Support support = new Support();
 
 
+    @SuppressLint("SetTextI18n")
     @Nullable
     @Override
     public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -69,12 +68,9 @@ public class FishBasket extends Fragment {
             tvFishBackToCatalog.setVisibility(View.INVISIBLE);
 
         }
-        tvFishBackToCatalog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), CategoryFish.class);
-                startActivity(intent);
-            }
+        tvFishBackToCatalog.setOnClickListener(view14 -> {
+            Intent intent = new Intent(getActivity(), CategoryFish.class);
+            startActivity(intent);
         });
 
         adapterFish = new AdapterFish(getActivity(), cartItems);
@@ -82,49 +78,38 @@ public class FishBasket extends Fragment {
         lvShopBasket.setAdapter(adapterFish);
         cartItemOnClick();
 
-        lvShopBasket.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int i, long l) {
+        lvShopBasket.setOnItemLongClickListener((adapterView, view13, i, l) -> {
 
-                final Dialog dialogDeleteItem = new Dialog(getActivity(), R.style.FullHeightDialog);
-                dialogDeleteItem.setContentView(R.layout.dialog_delete_item);
-                dialogDeleteItem.getWindow().setBackgroundDrawable(new ColorDrawable (Color.TRANSPARENT));
+            final Dialog dialogDeleteItem = new Dialog(getActivity(), R.style.FullHeightDialog);
+            dialogDeleteItem.setContentView(R.layout.dialog_delete_item);
+            dialogDeleteItem.getWindow().setBackgroundDrawable(new ColorDrawable (Color.TRANSPARENT));
 
-                Button btnDeleteCancel = dialogDeleteItem.findViewById(R.id.cancel_btn_dialog_deleteItem);
-                btnDeleteCancel.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View view) {
-                        dialogDeleteItem.dismiss();
-                    }
-                });
-                Button btnDeleteItem = dialogDeleteItem.findViewById(R.id.ok_btn_dialog_deleteItem);
-                btnDeleteItem.setOnClickListener(new View.OnClickListener() {
-                    @SuppressLint("SetTextI18n")
-                    @Override
-                    public void onClick(View view) {
+            Button btnDeleteCancel = dialogDeleteItem.findViewById(R.id.cancel_btn_dialog_deleteItem);
+            btnDeleteCancel.setOnClickListener(view12 -> dialogDeleteItem.dismiss());
+            Button btnDeleteItem = dialogDeleteItem.findViewById(R.id.ok_btn_dialog_deleteItem);
+            btnDeleteItem.setOnClickListener(view1 -> {
 
-                        cartItems.remove(i);
-                        btnOrder.setText ("Купить за " + CartHelper.finalSumOrder(cartItems , cartEquipmentItem)+ " грн.");
-                        adapterFish = new AdapterFish(getContext(), cartItems);
-                        lvShopBasket.setAdapter(adapterFish);
-                        int cartItemText = Integer.parseInt((String) cartAddItemText.getText());
-                        String newCartItemText = String.valueOf((cartItemText-1));
-                        cartAddItemText.setText(newCartItemText);
-                        if(cartItems.isEmpty()){
-                            tvFishNotItemsCart.setVisibility(View.VISIBLE);
-                            tvFishBackToCatalog.setVisibility(View.VISIBLE);
-                        }
-                        if (cartEquipmentItem.isEmpty() && cartItems.isEmpty ())
-                        {
-                            btnOrder.setVisibility (View.INVISIBLE);
-                        }
-                        dialogDeleteItem.dismiss();
-                    }
-                });
-                dialogDeleteItem.setCancelable(false);
-                dialogDeleteItem.show();
+                cartItems.remove(i);
+                btnOrder.setText ("Купить за " + CartHelper.finalSumOrder(cartItems , cartEquipmentItem)+ " грн.");
+                adapterFish = new AdapterFish(getContext(), cartItems);
+                lvShopBasket.setAdapter(adapterFish);
+                int cartItemText = Integer.parseInt((String) cartAddItemText.getText());
+                String newCartItemText = String.valueOf((cartItemText-1));
+                cartAddItemText.setText(newCartItemText);
+                if(cartItems.isEmpty()){
+                    tvFishNotItemsCart.setVisibility(View.VISIBLE);
+                    tvFishBackToCatalog.setVisibility(View.VISIBLE);
+                }
+                if (cartEquipmentItem.isEmpty() && cartItems.isEmpty ())
+                {
+                    btnOrder.setVisibility (View.INVISIBLE);
+                }
+                dialogDeleteItem.dismiss();
+            });
+            dialogDeleteItem.setCancelable(false);
+            dialogDeleteItem.show();
 
-                return true;
-            }
+            return true;
         });
 
         hideKeyboard();
@@ -134,70 +119,54 @@ public class FishBasket extends Fragment {
 
     @SuppressLint("ClickableViewAccessibility")
     private void hideKeyboard() {
-        lvShopBasket.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                InputMethodManager inputMethodManager = (InputMethodManager) Objects.requireNonNull(getActivity()).getSystemService(Activity.INPUT_METHOD_SERVICE);
-                inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
-                return false;
-            }
+        lvShopBasket.setOnTouchListener((view, motionEvent) -> {
+            InputMethodManager inputMethodManager = (InputMethodManager) Objects.requireNonNull(getActivity()).getSystemService(Activity.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
+            return false;
         });
     }
 
 
     private void cartItemOnClick (){
-        lvShopBasket.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
-                final Dialog dialog = new Dialog(getActivity(), R.style.FullHeightDialog);
-                dialog.setContentView(R.layout.dialog_edit_quantity);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable (Color.TRANSPARENT));
+        lvShopBasket.setOnItemClickListener((adapterView, view, i, l) -> {
+            final Dialog dialog = new Dialog(getActivity(), R.style.FullHeightDialog);
+            dialog.setContentView(R.layout.dialog_edit_quantity);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable (Color.TRANSPARENT));
 
-                final EditText editQuantity = dialog.findViewById(R.id.quantity_edit_dialog);
-                final Button btnCancelDialog = dialog.findViewById(R.id.cancel_dialog_quantity__btn);
-                btnCancelDialog.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        dialog.dismiss();
-                    }
-                });
+            final EditText editQuantity = dialog.findViewById(R.id.quantity_edit_dialog);
+            final Button btnCancelDialog = dialog.findViewById(R.id.cancel_dialog_quantity__btn);
+            btnCancelDialog.setOnClickListener(view1 -> dialog.dismiss());
 
-                updateCartItemShop(i, dialog, editQuantity);
-                dialog.show();
-
-            }
+            updateCartItemShop(i, dialog, editQuantity);
+            dialog.show();
 
         });
 
     }
     private void updateCartItemShop(final int i, final Dialog dialog, final EditText editQuantity) {
         final Button btnEditQuantity = dialog.findViewById(R.id.edit_quantity_btn);
-        btnEditQuantity.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View view) {
+        btnEditQuantity.setOnClickListener(view -> {
 
-                String quantityFish = editQuantity.getText().toString();
+            String quantityFish = editQuantity.getText().toString();
 
-                if (editQuantity.length () < 1) {
-                    Support support = new Support();
-                    support.showToast (getContext(), "Укажите количество");
-                    return;
-                }
-
-                cartItems.get(i).put("quantity", quantityFish);
-
-                ArrayList<HashMap<String, String> > editListQuantity = cartItems;
-
-                editListQuantity.get(i).put("quantity", quantityFish);
-
-                btnOrder.setText ("Сумма покупки " + CartHelper.finalSumOrder(cartItems , cartEquipmentItem)+ " грн.");
-
-                adapterFish = new AdapterFish(getContext(), editListQuantity);
-                lvShopBasket.setAdapter(adapterFish);
-
-                dialog.dismiss();
+            if (editQuantity.length () < 1) {
+                Support support = new Support();
+                support.showToast (getContext(), "Укажите количество");
+                return;
             }
+
+            cartItems.get(i).put("quantity", quantityFish);
+
+            ArrayList<HashMap<String, String> > editListQuantity = cartItems;
+
+            editListQuantity.get(i).put("quantity", quantityFish);
+
+            btnOrder.setText ("Сумма покупки " + CartHelper.finalSumOrder(cartItems , cartEquipmentItem)+ " грн.");
+
+            adapterFish = new AdapterFish(getContext(), editListQuantity);
+            lvShopBasket.setAdapter(adapterFish);
+
+            dialog.dismiss();
         });
     }
 }
